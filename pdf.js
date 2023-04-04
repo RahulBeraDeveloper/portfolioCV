0ea352f2aa4cc01f5df4b55f2d9ea0c8f0e0868c
Rahul Bera <rahaulbera3654@gmail.com>
2023-04-04 12:52:36 +0530

    first comment

---
+++ pdf.js
@@ -0,0 +1,23 @@
+
+// function generatePDF(){
+//   const element =document.getElementById("resume");
+//   html2pdf()
+//   .from(element)
+//   .save();
+// }
+window.onload=function(){
+  document.getElementById("download")
+  .addEventListener("click",()=>{
+    const resume =this.document.getElementById("resume");
+    console.log(resume);
+    console.log(window);
+    var opt = {
+      margin:       1,
+      filename:     'myfile.pdf',
+      image:        { type: 'jpeg', quality: 0.98 },
+      html2canvas:  { scale: 2 },
+      jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+    html2pdf().from(resume).set(opt).save();
+  })
+}
